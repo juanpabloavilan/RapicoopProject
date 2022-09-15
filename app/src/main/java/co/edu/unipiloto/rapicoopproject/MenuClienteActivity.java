7ea3bb5f9540a3389b70404a8623ece249c0cbfc
Juan Pablo <avilanjuanpablo@gmail.com>
2022-09-15 18:53:25 -0500

    Se crearon activity_menu_cliente.xml y activity_menu_vendedor.xml

---
+++ app/src/main/java/co/edu/unipiloto/rapicoopproject/MenuClienteActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.rapicoopproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_cliente);
+    }
+}
