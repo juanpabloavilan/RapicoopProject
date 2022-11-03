ddca704b80709578e9cab6d730787b2f025c34bc
Juan Pablo <avilanjuanpablo@gmail.com>
2022-11-03 16:42:07 -0500

    Feature agregar platos al menú agregado

---
+++ app/src/main/java/co/edu/unipiloto/rapicoopproject/AddMenuDishActivity.java
@@ -0,0 +1,71 @@
+package co.edu.unipiloto.rapicoopproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import co.edu.unipiloto.rapicoopproject.applicationcontext.UserLoggedContext;
+import co.edu.unipiloto.rapicoopproject.entities.MenuDishFacade;
+import co.edu.unipiloto.rapicoopproject.lib.MenuDish;
+import co.edu.unipiloto.rapicoopproject.lib.User;
+
+public class AddMenuDishActivity extends AppCompatActivity {
+    private Button btnAddMenuDish;
+    private EditText eTxtNombre;
+    private EditText eTxtDescripcion;
+    private EditText eTxtPrecio;
+    private EditText eTxtFoodCategory;
+
+    private User userLogged;
+    private MenuDishFacade menuDishFacade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_menu_dish);
+
+        eTxtNombre = findViewById(R.id.etxt_nombre_plato_value);
+        eTxtDescripcion = findViewById(R.id.etxt_descripcion_value);
+        eTxtPrecio = findViewById(R.id.etxt_precio_value);
+        eTxtFoodCategory = findViewById(R.id.etxt_food_category_value);
+        btnAddMenuDish = findViewById(R.id.btn_add_menu_dish);
+
+        userLogged = UserLoggedContext.getInstance().getUser();
+        menuDishFacade = MenuDishFacade.getInstance(this);
+
+        btnAddMenuDish.setOnClickListener((v)->{
+            addMenuDish();
+        });
+
+
+
+    }
+
+    private void addMenuDish() {
+        String nombreDelPlato = eTxtNombre.getText().toString();
+        String descripcion = eTxtDescripcion.getText().toString();
+        Integer precio = Integer.parseInt(eTxtPrecio.getText().toString());
+        String foodCategory = eTxtFoodCategory.getText().toString();
+        Integer vendorID = userLogged.getId();
+
+        boolean areFieldsValid = !nombreDelPlato.equals("") &&
+                !descripcion.equals("") &&
+                precio > 0 &&
+                !foodCategory.equals("");
+
+        if(areFieldsValid){
+            MenuDish menuDish = new MenuDish(descripcion, nombreDelPlato,precio, R.drawable.add_menu_icon);
+            menuDish.setFoodCategory(foodCategory);
+            menuDish.setVendorId(vendorID);
+            long id = menuDishFacade.insertMenuDish(menuDish);
+            menuDish.setId((int) id);
+            Toast.makeText(this, "Plato agregado al menú correctamente con id "+ id, Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Datos invalidos ingresados, por favor intente de nuevo", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
--- app/src/main/java/co/edu/unipiloto/rapicoopproject/MiMenuActivity.java
+++ app/src/main/java/co/edu/unipiloto/rapicoopproject/MiMenuActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,16 +23,30 @@ public class MiMenuActivity extends AppCompatActivity {
     RecyclerView rvListaPlatos;
     MenuAdapter menuAdapter;
     User userLogged;
+    Button btnGoToAddMenuDish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_menu);
-        menuDishFacade = MenuDishFacade.getInstance(this);
+        btnGoToAddMenuDish = findViewById(R.id.btn_go_to_add_menu_dish);
         rvListaPlatos = findViewById(R.id.rv_menu_dishes_list);
+
+        menuDishFacade = MenuDishFacade.getInstance(this);
         userLogged = UserLoggedContext.getInstance().getUser();
         loadDishesListToView();
 
+        btnGoToAddMenuDish.setOnClickListener(this::goToAddMenuDish);
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
     }
 
     private void loadDishesListToView () {
@@ -37,5 +54,10 @@ public class MiMenuActivity extends AppCompatActivity {
         menuAdapter = new MenuAdapter(this, listaPlatos);
         rvListaPlatos.setAdapter(menuAdapter);
         rvListaPlatos.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void goToAddMenuDish(View view){
+        Intent goToAddMenuDishActivity = new Intent(this, AddMenuDishActivity.class);
+        startActivity(goToAddMenuDishActivity);
     }
 }
